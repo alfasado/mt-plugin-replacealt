dfe7dcf20dcbf10ec9f9821c08cdccbc73a16cfe
Junnama Noda <junnama@alfasado.jp>
2015-07-29 18:29:43 +0900

    first commit.

---
+++ plugins/ReplaceAlt/lib/ReplaceAlt/Callbacks.pm
@@ -0,0 +1,45 @@
+package ReplaceAlt::Callbacks;
+
+use strict;
+use warnings;
+
+sub _cb_cms_post_save_page {
+    my ( $cb, $app, $obj, $original ) = @_;
+    my $text = $obj->text;
+    my $text_more = $obj->text_more;
+    $text =~ s{
+                 (
+                   <img
+                     (?=[^>]*?\s+alt\s*=\s*"([^"]+)")
+                     (?=[^>]*?\s+src\s*=\s*"(?:[^"]*?/)?\2")
+                 )
+                     \s+alt\s*=\s*"[^"]+"
+                 (   (?:\s[^>]*)?> \s* )
+                 <p(?:\s[^>]*)?>(.+?)</p\s*>
+             }{
+                 my($m1, $p_html, $m3) = ($1, $4, $3);
+                 $p_html =~ s/<[^>]*>//g;
+                 $p_html =~ s/"/&quot;/g;
+                 qq{$m1 alt="$p_html"$m3}
+             }gsex;
+    $text_more =~ s{
+                 (
+                   <img
+                     (?=[^>]*?\s+alt\s*=\s*"([^"]+)")
+                     (?=[^>]*?\s+src\s*=\s*"(?:[^"]*?/)?\2")
+                 )
+                     \s+alt\s*=\s*"[^"]+"
+                 (   (?:\s[^>]*)?> \s* )
+                 <p(?:\s[^>]*)?>(.+?)</p\s*>
+             }{
+                 my($m1, $p_html, $m3) = ($1, $4, $3);
+                 $p_html =~ s/<[^>]*>//g;
+                 $p_html =~ s/"/&quot;/g;
+                 qq{$m1 alt="$p_html"$m3}
+             }gsex;
+    $obj->text( $text );
+    $obj->text_more( $text_more );
+    $obj->save || die $obj->errstr;
+}
+
+1;
---
+++ plugins/ReplaceAlt/lib/ReplaceAlt/L10N/ja.pm
@@ -0,0 +1,11 @@
+package ReplaceAlt::L10N::ja;
+use strict;
+use base 'ReplaceAlt::L10N';
+
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'It will replace the alt attribute value of the img element in the text immediately following the paragraph .' => '直後の段落のテキストでimg要素のalt属性値を置換します。',
+    );
+
+1;
